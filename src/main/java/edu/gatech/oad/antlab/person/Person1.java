72133362263d3f514467ddcdaa67080a0204b93b
vvinutna <vinutna.veeragandham@gmail.com>
2017-01-24 00:34:43 -0500

    fixed a typo

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  char[] original = input.tocharArray();
+	  char[] original = input.toCharArray();
 	  String modified = "";
 	  for(int i = 2; i < original.length; i++) {
 	  	modified += original[i];
